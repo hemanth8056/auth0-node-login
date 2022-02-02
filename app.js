d6a895a1def3f6bdf9a2b99998257234d3829fab
hemanth8056 <77633475+hemanth8056@users.noreply.github.com>
2022-02-02 10:55:05 -0600

    changes donein app.js

--- app.js
+++ app.js
@@ -15,31 +15,29 @@ var usersRouter = require('./routes/users');
 dotenv.config();
 
 // Configure Passport to use Auth0
-var strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  },
-  function (accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
+var strategy = new Auth0Strategy({
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
 );
 
 passport.use(strategy);
 
 // You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-  done(null, user);
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+passport.deserializeUser(function(user, done) {
+    done(null, user);
 });
 
 const app = express();
@@ -55,23 +53,23 @@ app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 // config express-session
 var sess = {
-  secret: 'CHANGE THIS SECRET',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true
+    secret: 'CHANGE THIS SECRET',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
 };
 
 if (app.get('env') === 'production') {
-  // If you are using a hosting provider which uses a proxy (eg. Heroku),
-  // comment in the following app.set configuration command
-  //
-  // Trust first proxy, to prevent "Unable to verify authorization request state."
-  // errors with passport-auth0.
-  // Ref: https://github.com/auth0/passport-auth0/issues/70#issuecomment-480771614
-  // Ref: https://www.npmjs.com/package/express-session#cookiesecure
-  // app.set('trust proxy', 1);
-  
-  sess.cookie.secure = true; // serve secure cookies, requires https
+    // If you are using a hosting provider which uses a proxy (eg. Heroku),
+    // comment in the following app.set configuration command
+    //
+    // Trust first proxy, to prevent "Unable to verify authorization request state."
+    // errors with passport-auth0.
+    // Ref: https://github.com/auth0/passport-auth0/issues/70#issuecomment-480771614
+    // Ref: https://www.npmjs.com/package/express-session#cookiesecure
+    app.set('trust proxy', 1);
+
+    sess.cookie.secure = true; // serve secure cookies, requires https
 }
 
 app.use(session(sess));
@@ -83,14 +81,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 
 // Handle auth failure error messages
-app.use(function (req, res, next) {
-  if (req && req.query && req.query.error) {
-    req.flash('error', req.query.error);
-  }
-  if (req && req.query && req.query.error_description) {
-    req.flash('error_description', req.query.error_description);
-  }
-  next();
+app.use(function(req, res, next) {
+    if (req && req.query && req.query.error) {
+        req.flash('error', req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash('error_description', req.query.error_description);
+    }
+    next();
 });
 
 app.use(userInViews());
@@ -99,10 +97,10 @@ app.use('/', indexRouter);
 app.use('/', usersRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Error handlers
@@ -110,23 +108,23 @@ app.use(function (req, res, next) {
 // Development error handler
 // Will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // Production error handler
 // No stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
